ed00f22a35716cf2ed106db8c76d26f1e43b80fd
N Dhivija <ndhivija@gmail.com>
2026-07-30 15:13:02 +0530

    Fix formatting of server_port environment variable

--- app.py
+++ app.py
@@ -37,5 +37,5 @@ demo=gr.Interface(
 if __name__ == "__main__":
   demo.launch(
     server_name="0.0.0.0",
-    server_port=int(os.environ.get("PORT, 7860))
+    server_port=int(os.environ.get("PORT", 7860))
   )
